e11b8a9d3e39f044bf524053a19e769d37c56f81
peyceball <vorobev.iv@phystech.edu>
2021-03-14 16:36:08 +0300

    add shebang

--- call_pylint.sh
+++ call_pylint.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 score=$(find -name '*.py' | xargs pylint | tail -n 2 | head -n 1 | awk '{print $7}' | awk -F/ '{print $1}' | xargs)
 if (($(echo $score | awk -F. '{print $1}') < 9))
 then
